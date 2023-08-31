0dba1bb7ae39eba93951a9ca7172ba31388c2c59
Aditi Saxena <90380587+aditisaxena29@users.noreply.github.com>
2023-08-31 23:24:32 +0530

    CORS added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
  import cookieParser from "cookie-parser";
-// import cors from "cors";
+ import cors from "cors";
 
 config({
   path: "./config/config.env",
@@ -17,13 +17,13 @@ app.use(
   })
 );
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //Importing & Using Routes
 import course from "./routes/courseRoutes.js";
