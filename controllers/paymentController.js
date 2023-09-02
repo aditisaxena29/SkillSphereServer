302500d150eca4c1913336fbd051af2211d022d6
Aditi Saxena <90380587+aditisaxena29@users.noreply.github.com>
2023-09-02 15:25:14 +0530

    Updated payment.remove() in paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -93,7 +93,7 @@ export const cancelSubscription = catchAsyncError(async (req, res, next) => {
     refund = true;
   }
 
-  await payment.remove();
+  await payment.deleteOne();
   user.subscription.id = undefined;
   user.subscription.status = undefined;
   await user.save();
